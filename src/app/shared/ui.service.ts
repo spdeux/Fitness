5d4e44818e823f533bb087af79ed08d2f7b79329
spdeux <sepideh_masoomi@yahoo.com>
2019-09-27 08:41:12 -0400

    fix error

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,6 +1,9 @@
 import { Subject } from "rxjs";
 import { MatSnackBar } from '@angular/material';
+import { Injectable } from '@angular/core';
 
+
+@Injectable()
 export class UIService {
     loadingStateChanged = new Subject<boolean>();
 
